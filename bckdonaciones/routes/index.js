d8059173f58b4949fe6c97ce005b37c376c21207
Monica <moniportillo2014@gmail.com>
2020-06-30 15:17:29 -0600

    Nuevo2.1

--- bckdonaciones/routes/index.js
+++ bckdonaciones/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var usuariosRouter = require('./Api/Usuarios/index');
-var donacionesRauter = require('./Api/donaciones/donaciones');
-var secRauter = require('./Api/Sec/index');
+var donacionesRouter = require('./Api/donaciones/donaciones');
+var secRouter = require('./Api/Sec/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +10,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.use('/api/usuarios', usuariosRouter);
-router.use('/api/Donaciones', donacionesRauter);
-router.use('/api/Sec', secRauter);
+router.use('/api/Donaciones', donacionesRouter);
+router.use('/api/Sec', secRouter);
 module.exports = router;
