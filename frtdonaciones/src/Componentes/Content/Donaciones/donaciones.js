78b4ca59fa0a8a848c1d2e0a3f5e539c5d55f534
Breydi31 <almendaresbreydi@gmail.com>
2020-08-02 16:22:22 -0600

    Ay vamosss

--- frtdonaciones/src/Componentes/Content/Donaciones/donaciones.js
+++ frtdonaciones/src/Componentes/Content/Donaciones/donaciones.js
@@ -1,64 +1,178 @@
-/*import React, {Component} from 'react';
+import React, {Component} from 'react';
 import Page from '../../Page';
-import { Redirect } from 'react-router-dom';
-import {paxios} from '../../../utilities/axios';
+import axios from 'axios'; //Para estas dos instale npm i bootstrap reactstrap axios
+import "bootstrap/dist/css/bootstrap.min.css";
 
-//import './PlatilloDetail.css';
-export default class DonacionDetail extends Component{
-  constructor(){
-    super();
-    this.state = {}
-    this.addMoreStock = this.addMoreStock.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
+
+//import './donaciones.css';
+//import {obtenerDonacion} from './actions';
+import { NavLink } from 'react-router-dom';
+import { IoIosInformationCircle } from 'react-icons/io'
+
+class Donacion extends Component {
+  state={
+    data:[],
+    modalInsertar: false,
+    modalEliminar: false,
+    form:{
+      id: '',
+      nombre: '',
+      correo: '',
+      celular: '',
+      tipoModal: ''
+    }
   }
-  //Encontrar producto
-  componentDidMount()
-  {
-    const prodId = this.props.match.params.id;
-    paxios.get(
-      `/api/donaciones/donaciones/one/${prodId}`
-    )
-    .then((data)=>{
-      console.log(data);
-      this.setState(data.data);
-      console.log(this.state);
+  
+  peticionGet=()=>{
+    const url="https://localhost:3000/api/Donaciones/all";
+  axios.get(url).then(response=>{
+    this.setState({data: response.data});
+  }).catch(error=>{
+    console.log(error.message);
+  })
+  }
+  
+  peticionPost=async()=>{
+    const url="https://localhost:3000/api/Donaciones/add";
+    delete this.state.form.id;
+   await axios.post(url,this.state.form).then(response=>{
+      this.modalInsertar();
+      this.peticionGet();
+    }).catch(error=>{
+      console.log(error.message);
     })
-    .catch((e)=>{
-      console.log(e);
+  }
+  
+  peticionPut=()=>{
+      const url="https://localhost:3000/api/Donaciones/upd/:id";
+    axios.put(url+this.state.form.id, this.state.form).then(response=>{
+      this.modalInsertar();
+      this.peticionGet();
     })
   }
-  //Funcion agregar stock
-  addMoreStock(e){
-    e.preventDefault();
-    e.stopPropagation();
-    const prodId = this.props.match.params.id;
-    paxios.put(
-      `/api/donaciones/donaciones/new/${prodId}`,
-      {stock: 1}
-    )
-      .then((data) => {
-        this.setState(data.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      })
+  
+  peticionDelete=()=>{
+      const url="https://localhost:3000/api/Donaciones/del/:id";
+    axios.delete(url+this.state.form.id).then(response=>{
+      this.setState({modalEliminar: false});
+      this.peticionGet();
+    })
   }
-  //Renderizar pantalla
-  render(){
-      const id = this.props.match.params.id;
-      if(!(id && true)){
-        return (<Redirect to="/donaciones"/>)
+  
+  modalInsertar=()=>{
+    this.setState({modalInsertar: !this.state.modalInsertar});
+  }
+  
+  seleccionarDonaciones=(Donaciones)=>{
+    this.setState({
+      tipoModal: 'actualizar',
+      form: {
+        id: Donaciones.id,
+        nombre: Donaciones.nombre,
+        correo: Donaciones.correo,
+        celular: Donaciones.celular
       }
-      var {pid,sku,nombre, correo,celular,Estado,fecha} = this.state;
-      return (
-        <Page pageTitle={nombre} auth={this.props.auth}>
-          <span className="detailitem">{nombre}</span>
-          <span className="detailitem">{correo}</span>
-          <span className="detailitem">{celular}</span>
-          <fieldset>
-          <button onClick={this.addMoreStock}>Add One more Stock +</button>
-          </fieldset>
-        </Page>
-      )
+    })
   }
-} */
+  
+  handleChange=async e=>{
+  e.persist();
+  await this.setState({
+    form:{
+      ...this.state.form,
+      [e.target.name]: e.target.value
+    }
+  });
+  console.log(this.state.form);
+  }
+  
+    componentDidMount() {
+      this.peticionGet();
+    }
+    
+  
+    render(){
+      const {form}=this.state;
+    return (
+      <div className="App">
+      <br /><br /><br />
+    <button className="btn btn-success" onClick={()=>{this.setState({form: null, tipoModal: 'insertar'}); this.modalInsertar()}}>Agregar Donación</button>
+    <br /><br />
+      <table className="table ">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Celular</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.data.map(Donaciones=>{
+            return(
+              <tr>
+            <td>{Donaciones.id}</td>
+            <td>{Donaciones.nombre}</td>
+            <td>{Donaciones.correo}</td>
+            <td>{Donaciones.celular}</td>
+            <td>
+                  <button className="btn btn-primary" onClick={()=>{this.seleccionarDonaciones(Donaciones); this.modalInsertar()}}></button>
+                  {"   "}
+                  <button className="btn btn-danger" onClick={()=>{this.seleccionarDonaciones(Donaciones); this.setState({modalEliminar: true})}}></button>
+                  </td>
+            </tr>
+            )
+          })}
+        </tbody>
+      </table>
+  
+  
+  
+      <Modal isOpen={this.state.modalInsertar}>
+                  <ModalHeader style={{display: 'block'}}>
+                    <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="form-group">
+                      <label htmlFor="id">ID</label>
+                      <input className="form-control" type="text" name="id" id="id" readOnly onChange={this.handleChange} value={form?form.id: this.state.data.length+1}/>
+                      <br />
+                      <label htmlFor="nombre">Nombre</label>
+                      <input className="form-control" type="text" name="nombre" id="nombre" onChange={this.handleChange} value={form?form.nombre: ''}/>
+                      <br />
+                      <label htmlFor="correo">Correo</label>
+                      <input className="form-control" type="text" name="correo" id="correo" onChange={this.handleChange} value={form?form.correo: ''}/>
+                      <br />
+                      <label htmlFor="celular">celular</label>
+                      <input className="form-control" type="number" name="celular" id="celular" onChange={this.handleChange} value={form?form.celular:''}/>
+                    </div>
+                  </ModalBody>
+  
+                  <ModalFooter>
+                    {this.state.tipoModal=='insertar'?
+                      <button className="btn btn-success" onClick={()=>this.peticionPost()}>
+                      Insertar
+                    </button>: <button className="btn btn-primary" onClick={()=>this.peticionPut()}>
+                      Actualizar
+                    </button>
+    }
+                      <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+                  </ModalFooter>
+            </Modal>
+  
+  
+            <Modal isOpen={this.state.modalEliminar}>
+              <ModalBody>
+                 Estás seguro que deseas eliminar la Donación {form && form.nombre}
+              </ModalBody>
+              <ModalFooter>
+                <button className="btn btn-danger" onClick={()=>this.peticionDelete()}>Sí</button>
+                <button className="btn btn-secundary" onClick={()=>this.setState({modalEliminar: false})}>No</button>
+              </ModalFooter>
+            </Modal>
+    </div>
+    );
+  }
+}
+export default Donacion;
